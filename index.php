c3e8ae33c15853082f9a4365134259978babf164
Nokx1z <enyelldev@gmail.com>
2026-03-29 16:41:15 -0400

    Inicialización de la arquitectura MVC

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sys Inescolara</title>
+</head>
+<body>
+    <h1>Sys Inescolara</h1>
+</body>
+</html>
