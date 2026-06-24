0dc71e6705b5ad676089ad27d980d591ea064090
anyelber <anyelberalvarado23@gmail.com>
2026-06-24 10:55:09 -0400

    estilos en tablas y letras

--- app/views/common/links.php
+++ app/views/common/links.php
@@ -36,6 +36,7 @@ $scripts_links = '
     <!-- DataTables JS -->
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+    <script>$.extend(true, $.fn.dataTable.defaults, { order: [] });</script>
     
     <!-- SweetAlert2 JS -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
